e4ade4da5823a5e9466b7836d511b395ad9abec8
Zach M <zmorse@student.uiwtx.edu>
2022-03-15 13:39:56 -0500

    Heavily edited activity1
    Let's see if this works

--- iotCodeBase/activity1.py
+++ iotCodeBase/activity1.py
@@ -1,22 +1,22 @@
 import RPi.GPIO as GPIO
 import time
 
-def button_callback(channel):
-    while True:
-        input_value = GPIO.input(18)
-
-        if input_value == True:
-            pressed += 1;
-            time = 0; #to start the counter at 0
-
-        if (time > 10): #you wait 1 sec between each presure
-            print("the button has been pressed " + pressed + " times");
-            pressed = 0; # you don't count anymore
-
-        if (pressed > 0): # you are pressing the button so you count
-            time += 1;
-
-        time.sleep(0.1)
+def button_press(channel):
+    channel = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=1500)
+    if channel is None:
+        print('LED 1')
+    else:
+        channel = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=1500)
+        if channel is None:
+            print('LED 2')
+        else:
+            channel = GPIO.wait_for_edge(channel, GPIO.RISING, timeout=1500)
+            if channel is None:
+                print('LED 3')
+                time.sleep(3)
+            else:
+                print('Too many button presses!')
+                time.sleep(3)
 
 
 GPIO.setmode(GPIO.BCM)
@@ -26,7 +26,7 @@ GPIO.setup(3,GPIO.OUT)
 GPIO.setup(4,GPIO.OUT)
 GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(17,GPIO.RISING,callback=button_callback) # Setup event on pin 17 rising edge
+GPIO.add_event_detect(17,GPIO.RISING,callback=button_press,bouncetime=8000) # Setup press event on pin 17
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
