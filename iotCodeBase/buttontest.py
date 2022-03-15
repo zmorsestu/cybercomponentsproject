e5046c1e8a3de512c1d0b4aef118fc77449f3b21
Zach M <zmorse@student.uiwtx.edu>
2022-03-15 12:26:07 -0500

    Added comments to test scripts
    Hopefully everyone can pretty easily pick up on what's going on there

--- iotCodeBase/buttontest.py
+++ iotCodeBase/buttontest.py
@@ -1,17 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-def button_callback(channel):
-    print("Button was pushed!")
-
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+#setup LED pinout
 GPIO.setup(2,GPIO.OUT)
 GPIO.setup(3,GPIO.OUT)
 GPIO.setup(4,GPIO.OUT)
+#setup button pinout
 GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
-GPIO.add_event_detect(17,GPIO.RISING,callback=button_callback) # Setup event on pin 17 rising edge
+#create a callback for when the button is pressed
+def button_callback(channel):
+    print("Button was pushed!")
+
+#using GPIO events, do this callback when the button is pressed
+GPIO.add_event_detect(17,GPIO.RISING,callback=button_callback)
 
 message = input("Press enter to quit\n\n") # Run until someone presses enter
 
--- iotCodeBase/lightTest.py
+++ iotCodeBase/lightTest.py
@@ -2,11 +2,14 @@ import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+#set up LED pinout
 GPIO.setup(2,GPIO.OUT)
 GPIO.setup(3,GPIO.OUT)
 GPIO.setup(4,GPIO.OUT)
+#set up button as input
 GPIO.setup(17,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
 
+#endless loop that should flash the lights
 while True:
     GPIO.output(2,GPIO.HIGH)
     time.sleep(1)
